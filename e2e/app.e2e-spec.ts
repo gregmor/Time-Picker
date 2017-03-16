f45e021924001e4fe9b38ce2caeabf5c189fdc1b
Angular CLI <angular-cli@angular.io>
2017-03-16 15:03:34 -0700

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TimePickerPage } from './app.po';
+
+describe('time-picker App', () => {
+  let page: TimePickerPage;
+
+  beforeEach(() => {
+    page = new TimePickerPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
